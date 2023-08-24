c1c3c3de06a138f2635b6a27b1e3e7b7fd42d53a
Zaheer Jan <zaheer@cloaked.id>
2023-08-25 04:37:04 +0500

    code cleanup in views file

--- edxapp/views.py
+++ edxapp/views.py
@@ -28,34 +28,3 @@ class GreetingViewSet(
             data = GreetingSerializer(instance=obj, context={'request': request}).data
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=False, methods=['post'])
-    def save_greeting(self, request):
-        greeting_text = request.data.get('greeting')
-        if greeting_text:
-            if request.user.is_authenticated:
-                user_id = request.user.id
-            else:
-                user_id = None
-
-            # Log the greeting
-            LogEntry.objects.log_action(
-                user_id=user_id,
-                content_type_id=None,
-                object_id=None,
-                object_repr='Greeting',
-                action_flag=2,  # Change this according to log action codes
-                change_message=f'Greeting: {greeting_text}'
-            )
-
-            # Save the greeting to the database
-            Greeting.objects.create(text=greeting_text)
-
-            # Check if greeting is "hello" and call the original greeting endpoint
-            if greeting_text.lower() == 'hello':
-                # Code here to call the original greeting endpoint with "goodbye"
-                pass  # Placeholder for the call
-
-            return Response({'message': 'Greeting saved successfully'})
-        else:
-            return Response({'message': 'Invalid greeting'}, status=400)
